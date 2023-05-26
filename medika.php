e5f41ff3e266c8d67b592b5601ebc450ce6fded2
Daniar Nur Amin <daniarnuramin1@gmail.com>
2023-05-26 10:20:19 +0700

    Update Medika

---
+++ medika.php
@@ -0,0 +1,14 @@
+<?php
+
+    $n = 35;
+    for ($i=1; $i < $n; $i++) { 
+        if ($i % 3 == 0 && $i % 5 == 0) {
+            echo "LiraMedika \n";
+        }elseif($i % 3 == 0){
+            echo "Lira \n";
+        }elseif($i % 5 == 0){
+            echo "Medika \n";
+        }else{
+            echo $i."\n";
+        }
+    }
